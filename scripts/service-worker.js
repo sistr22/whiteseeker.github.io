ee1d8b1671354bf6584e90f3a474a598f185195f
Damien Mabin <dmabin@google.com>
2015-12-18 08:05:28 +0000

    Fix notification disapearing on chrome desktop

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -35,7 +35,8 @@ self.addEventListener('push', function(event) {
       self.registration.showNotification(title, {  
         body: body,  
         icon: icon,  
-        tag: tag
+        tag: tag,
+        requireInteration: true
       })  
     );  
   }, function (errorObject) {
@@ -48,7 +49,7 @@ self.addEventListener('notificationclick', function(event) {
   // Android doesn't close the notification when you click on it  
   // See: http://crbug.com/463146  
   event.notification.close();
-  
+
   console.log("notifurl=" + notifurl);
 
   // This looks to see if the current is already open and  
